dfe4555e94f02ad67d314eff1d7e4ac55f52b432
Jono Hill <jono@hillnz.com>
2022-07-18 18:14:17 +1200

    update healthcheck

--- root/healthcheck.sh
+++ root/healthcheck.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-cat "$RCLONE_MOUNT_TARGET/config.yaml" >/dev/null
+for dir in blobs garbage temp trash; do
+    mount_dir="$STORAGE_DIR/$dir"
+    if ! mount | grep "$mount_dir" >/dev/null; then
+        exit 1
+    fi
+done
